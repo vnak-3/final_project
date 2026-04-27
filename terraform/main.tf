84bc78ffcf31af472354bbd4aece7583e22e139c
vnak-3 <mrznak88k@gmail.com>
2026-04-27 20:59:55 +0700

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "app_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["3.88.37.248/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
